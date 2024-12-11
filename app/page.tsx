ad9787d6bfed8bb44f818de8440d8f690436a840
Jared Gentry <jared.c.gentry@gmail.com>
2024-12-11 12:35:27 -0500

    reduce max width and try to increase max duration of server action

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { AddForm } from "./AddForm";
 import { ArticleCard } from "./ArticleCard";
 import { Article } from "./types";
 
+export const maxDuration = 60; // Applies to the server actions
+
 export default async function Home() {
   const supabase = await createClient();
   const { data: article_summaries } = await supabase
@@ -16,7 +18,7 @@ export default async function Home() {
   });
 
   return (
-    <main className="max-w-4xl mx-auto px-4">
+    <main className="max-w-2xl mx-auto px-4">
       <h1 className="sr-only">Article Summaries</h1>
       <div className="sticky top-0 bg-background/80 backdrop-blur-sm z-10 py-8">
         <AddForm />
